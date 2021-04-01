f0c7098a1b974faa4f91c37af6bb7de8aec32df2
ibere.tizio <ibere.tizio@tivit.com>
2021-04-01 10:06:13 -0300

    S

--- carrot/templates/carrot/index.vue
+++ carrot/templates/carrot/index.vue
@@ -521,7 +521,7 @@
         },
         fdttime(value) {
             if (value) {
-               return moment(String(value)).format(‘MM/DD/YYYY hh:mm’)
+               return moment(String(value)).format('MM/DD/YYYY hh:mm')
             }
         },
         displayTime (time) {
